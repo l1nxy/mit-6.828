50cb138181a6ce41b92ac4c0e623c9603e171a26
linxy <l1nxy.zy@gmail.com>
2020-06-09 23:58:42 +0800

    use tdd and complete the test1

--- user/nsh.c
+++ user/nsh.c
@@ -1,29 +1,69 @@
 #include "kernel/types.h"
 #include "user/user.h"
 
-#define MAXTOKEN 32
-#define BUFSIZE 100
+#define TOKENSIZE 32
+#define MAXCMDSIZE 10
 
-char buf[BUFSIZE];
-int bufp = 0;
+char token[TOKENSIZE];
+char cmd[MAXCMDSIZE][TOKENSIZE];
+int token_pos = 0;
+int cmd_pos = 0;
 
-char* read_line(void);
-int main(int argc, char *argv[]){
-    char* b = read_line();
-    printf("read:%s\n",b);
+int get_token();
+int run_cmd(char *cmd, char **argv)
+{
+    if (fork() == 0)
+    {
+        int ret = exec(cmd, argv);
+        if (ret == -1)
+            fprintf(3, "exec %s error\n", cmd);
+        exit(0);
+    }
+    else{
+        wait(0);
+        return 0;
+    }
+
+}
+int main(int argc, char *argv[])
+{
+    int read_ret;
+    while ((read_ret = get_token()))
+    {
+        strcpy(cmd[cmd_pos], token);
+        cmd_pos++;
+        if (read_ret == 1)
+            break;
+    }
+    char *icmd[MAXCMDSIZE] = {0};
+    for (int i = 0; i < cmd_pos; ++i)
+    {
+        ///TODO split command
+        /*if (strcmp(cmd[i], "|") == 0 || strcmp(cmd[i], "<") == 0 || strcmp(cmd[i], ">"))*/
+        /*{*/
+            /*break;*/
+        /*}*/
+        icmd[i] = cmd[i];
+    }
+    run_cmd(cmd[0], icmd);
     exit(0);
 }
 
-
-char* read_line(void){
+int get_token()
+{
     char c;
-    while(read(0,&c,sizeof c) != 0){
-        if(c == '\n') break;
-        if(c != ' ' || c != '\r'){
-            buf[bufp++] = c;
-        }
+    while (read(0, &c, sizeof c) != 0)
+    {
+        if (c == ' ' || c == '\n')
+            break;
+        token[token_pos++] = c;
+        if (token_pos == TOKENSIZE)
+            break;
     }
-    buf[bufp] = '\0';
-    bufp = 0;
-    return buf;
+    token[token_pos] = '\0';
+    token_pos = 0;
+    if (c == '\n')
+        return 1;
+    else
+        return 2;
 }
